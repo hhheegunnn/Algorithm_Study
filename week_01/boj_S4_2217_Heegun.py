575a427e0a43f7f8b34e64ba5ba1771db11047af
22sonamu <22sonamu@naver.com>
2020-10-28 01:07:44 +0900

    여정화

--- week_01/boj_S4_2217_Heegun.py
+++ week_01/boj_S4_2217_Heegun.py
@@ -1,29 +1,51 @@
-"""https://www.acmicpc.net/problem/2217"""
 
 
-""" 로프 """
-
-N = int(input())
-
-w_list = list(int(input()) for _ in range(N))
-
-w_list.sort(reverse = True)
-
-possible_weight = 0
-
-for i in range(len(w_list)):
-    tmp = w_list[i]*(i+1)
-    if possible_weight <= tmp:
-        possible_weight = tmp
-
-
+def solution(n, lost, reserve):
     
-
-print(possible_weight)
-
+    nlist = [1 for i in range(n)]
     
-
-
-
-
-
+    for r in reserve:
+        nlist[r-1] +=1
+        
+    for l in lost:
+        nlist[l-1] -= 1
+   
+    for k in range(n):
+        if nlist[k] == 2:
+            if k == n:
+                if nlist[k-1] == 0:
+                    nlist[k-1] += 1
+                    
+                else:
+                    pass
+            elif k == 0:
+                if nlist[k+1] == 0:
+                    nlist[k+1] += 1
+                else:
+                    pass
+                
+            else:
+                if nlist[k-1] == 0:
+                    nlist[k-1] += 1
+                
+                    continue
+                elif nlist[k+1] == 0:
+                    nlist[k+1] += 1
+                else :
+                    pass
+                
+        else :
+            pass
+                
+                
+        
+        
+    answer = 0
+    for a in nlist:
+        if a == 0:
+            pass
+        else :
+            answer += 1
+    print(nlist)
+    return answer
+solution(5, [2,4], [1,3,5])
