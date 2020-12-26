9fdaafb0e4f9451659ff697649e92d73fd4b777c
nayeonkiim <wkdrnsldk98@naver.com>
2020-12-26 14:55:16 +0900

    [김나연] boj 돌게임

---
+++ week_09/boj_S5_9655_Nayeonkiim.java
@@ -0,0 +1,19 @@
+package week9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_S5_9655_Nayeonkiim {
+	
+	public static void main(String[] args) throws Exception, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		if(N%2 == 0)
+			System.out.println("CY");
+		else
+			System.out.println("SK");
+	}
+}	
