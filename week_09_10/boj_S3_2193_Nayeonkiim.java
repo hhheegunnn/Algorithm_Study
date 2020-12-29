6d8d204cf76c73c2840534db33e03c83db378918
nayeonkiim <wkdrnsldk98@naver.com>
2020-12-29 17:31:39 +0900

    [김나연] boj 이친수

---
+++ week_09_10/boj_S3_2193_Nayeonkiim.java
@@ -0,0 +1,23 @@
+package week9;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj_S3_2193_Nayeonkiim {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		long[][] d = new long[N+1][2];
+		d[1][0] = 0;
+		d[1][1] = 1;
+		for(int i=2; i <= N; i++) {
+			d[i][0] += d[i-1][0] + d[i-1][1];
+			d[i][1] += d[i-1][0];
+		}
+		
+		System.out.println(d[N][0]+d[N][1]);
+	}
+
+}
