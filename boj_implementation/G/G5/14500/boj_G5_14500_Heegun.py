e024bf6f8e11e60dd7bc81e1d7d86d5ea50d5051
Heegun <khg878@gmail.com>
2021-01-05 14:13:12 +0900

    [김희건] boj 테트로미노 [try ~ except IndexError: 실전 사용 가능?]

--- boj_implementation/G/G5/14500/boj_G5_14500_Heegun.py
+++ boj_implementation/G/G5/14500/boj_G5_14500_Heegun.py
@@ -4,7 +4,7 @@
 """테트로미노"""
 
 # git add boj_implementation/G/G5/14500/boj_G5_14500_Heegun.py
-# git commit -m "[김희건] boj 테트로미노"
+# git commit -m "[김희건] boj 테트로미노 [try ~ except IndexError: 실전 사용 가능?]"
 
 def check_in(r,c):
     if 0<= r < N and 0<= c < M:
@@ -118,3 +118,56 @@ for _ in range(N):
 
 
 print(solution(N,M,board))
+
+
+
+# 실전에서 사용 가능? try ~ except IndexError:
+
+"""n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+tetromino = [
+    [(0,0), (0,1), (1,0), (1,1)], # ㅁ
+    [(0,0), (0,1), (0,2), (0,3)], # ㅡ
+    [(0,0), (1,0), (2,0), (3,0)], # ㅣ
+    [(0,0), (0,1), (0,2), (1,0)], 
+    [(1,0), (1,1), (1,2), (0,2)],
+    [(0,0), (1,0), (1,1), (1,2)], # ㄴ
+    [(0,0), (0,1), (0,2), (1,2)], # ㄱ
+    [(0,0), (1,0), (2,0), (2,1)],
+    [(2,0), (2,1), (1,1), (0,1)],
+    [(0,0), (0,1), (1,0), (2,0)], 
+    [(0,0), (0,1), (1,1), (2,1)],
+    [(0,0), (0,1), (0,2), (1,1)], # ㅜ
+    [(1,0), (1,1), (1,2), (0,1)], # ㅗ
+    [(0,0), (1,0), (2,0), (1,1)], # ㅏ
+    [(1,0), (0,1), (1,1), (2,1)], # ㅓ
+    [(1,0), (2,0), (0,1), (1,1)],
+    [(0,0), (1,0), (1,1), (2,1)],
+    [(1,0), (0,1), (1,1), (0,2)],
+    [(0,0), (0,1), (1,1), (1,2)]
+]
+
+
+def find(x, y):
+    global answer
+    for i in range(19):
+        result = 0
+        for j in range(4):
+            try:
+                next_x = x+tetromino[i][j][0] # x 좌표
+                next_y = y+tetromino[i][j][1] # y 좌표
+                result += board[next_x][next_y]
+            except IndexError:
+                continue
+        answer = max(answer, result)
+        
+def solve():
+    for i in range (n):
+        for j in range(m):
+            find(i, j)
+            
+answer = 0
+solve()
+print(answer)
+
+"""
